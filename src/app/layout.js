06cb526eec16896cf9c4f093ad31b403a3e7b15c
Alper Aktepe <87989578+alperaktepee@users.noreply.github.com>
2025-01-30 15:31:41 +0300

    removed script

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,11 +16,6 @@ export default function RootLayout({ children }) {
     <AuthProvider>
       <html lang="en">
     <Head>
-    <script   dangerouslySetInnerHTML={{__html:`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NQ6HJXNP');`}}/>
     </Head>
       <body className={inter.className}>
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NQ6HJXNP"
